5e06b0313bbb5f1234e2742ded7d86aa1c92346e
Christopher Fournier <chris.d.fournier@gmail.com>
2018-07-22 18:48:40 -0400

    Additional screens and master.scss setup
    Screens and styles for Claim Setup section: set password, fraud warning, and claim attachments.

--- assets/scripts/enservio.js
+++ assets/scripts/enservio.js
@@ -2,3 +2,11 @@
 var myElement = document.querySelector("header");
 var headroom  = new Headroom(myElement);
 headroom.init(); 
+
+// ATTACHMENT DELETE BUTTONS
+$(".item-one").click(function() {
+  $(".expanding-button-box.item-one").toggleClass("expand");
+});
+$(".item-two").click(function() {
+  $(".expanding-button-box.item-two").toggleClass("expand");
+});
