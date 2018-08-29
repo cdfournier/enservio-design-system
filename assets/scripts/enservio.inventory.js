d47062f69af2dd87f3507b33452c7072a805806c
Chris Fournier <chris.d.fournier@gmail.com>
2018-08-29 12:21:01 -0400

    Small screen updates; more Current Inventory work
    Made form guides "wider" for smaller screens; added Transitions to footer navigation on all pages; isolated Inventory Capture scripts to another .js file; modified Current Inventory script and css to improve UX.

---
+++ assets/scripts/enservio.inventory.js
@@ -0,0 +1,49 @@
+// CURRENT INVENTORY BAR BEHAVIOR
+$('.current-inventory-bar').click( function() {
+  $('.current-inventory').toggleClass('open');
+  $('body').toggleClass('lock');
+});
+
+
+// PIN/UNPIN CURRENT INVENTORY BAR
+$.fn.isOnScreen = function () {
+    var win = $(window);
+    var viewport = {
+        top: win.scrollTop(),
+        left: win.scrollLeft()
+    };
+    viewport.right = viewport.left + win.width();
+    viewport.bottom = viewport.top + win.height();
+    var bounds = this.offset();
+    bounds.right = bounds.left + this.outerWidth();
+    bounds.bottom = bounds.top + this.outerHeight();
+    return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
+};
+$(window).scroll(function () {
+    if ($('.release-inventory').isOnScreen() == true) {
+        $('.current-inventory').addClass('release');
+    }
+    else{
+        $('.current-inventory').removeClass('release');
+        $('.current-inventory').removeClass('open');
+    }
+});
+
+
+// STICK RELEASE-INVENTORY TO TOP
+$(window).scroll(function() {
+    var distanceFromTop = $(this).scrollTop();
+    if (distanceFromTop >= $('#create-inventory').height()) {
+        $('.release-inventory').addClass('fixed');
+    } else {
+        $('.release-inventory').removeClass('fixed');
+    }
+});
+
+
+// ADD ITEM TO INVENTORY ANIMATION
+$('.add-item').click( function() {
+  $('#first.inventory-capture').addClass('add');
+  $('.inventory-item.new').addClass('added');
+  $('.forms').addClass('new');
+});
--- assets/scripts/enservio.js
+++ assets/scripts/enservio.js
@@ -55,48 +55,6 @@ $('.delay.four').click(function() {
 });
 
 
-// CURRENT INVENTORY BAR BEHAVIOR
-$('.current-inventory-bar').click( function() {
-  $('.current-inventory').toggleClass('open');
-});
-
-
-// PIN/UNPIN CURRENT INVENTORY BAR
-$.fn.isOnScreen = function () {
-    var win = $(window);
-    var viewport = {
-        top: win.scrollTop(),
-        left: win.scrollLeft()
-    };
-    viewport.right = viewport.left + win.width();
-    viewport.bottom = viewport.top + win.height();
-    var bounds = this.offset();
-    bounds.right = bounds.left + this.outerWidth();
-    bounds.bottom = bounds.top + this.outerHeight();
-    return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
-};
-$(window).scroll(function () {
-    if ($('.release-inventory').isOnScreen() == true) {
-        $('.current-inventory').addClass('release');
-    }
-    else{
-        $('.current-inventory').removeClass('release');
-        $('.current-inventory').removeClass('open');
-    }
-});
-
-
-// STICK RELEASE-INVENTORY TO TOP
-$(window).scroll(function() {
-    var distanceFromTop = $(this).scrollTop();
-    if (distanceFromTop >= $('#create-inventory').height()) {
-        $('.release-inventory').addClass('fixed');
-    } else {
-        $('.release-inventory').removeClass('fixed');
-    }
-});
-
-
 // ALERT BARS
 $('.show-error').click( function() {
   $('.alert-bar.error').addClass('show');
@@ -142,14 +100,6 @@ $('.show-timeout').click( function() {
 });
 $('.continue').click( function() {
   $('.timeout').removeClass('show');
-});
-
-
-// ADD ITEM TO INVENTORY ANIMATION
-$('.add-item').click( function() {
-  $('#first.inventory-capture').addClass('add');
-  $('.inventory-item.new').addClass('added');
-  $('.forms').addClass('new');
 });
 
 
