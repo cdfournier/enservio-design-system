ac457c49cf3b633b463a2554a5c0de8386127c63
Christopher Fournier <chris.d.fournier@gmail.com>
2018-08-29 18:27:56 -0400

    $('body').toggleClass('lock') removed

--- assets/scripts/enservio.inventory.js
+++ assets/scripts/enservio.inventory.js
@@ -1,7 +1,7 @@
 // CURRENT INVENTORY BAR BEHAVIOR
 $('.current-inventory-bar').click( function() {
   $('.current-inventory').toggleClass('open');
-  $('body').toggleClass('lock');
+//   $('body').toggleClass('lock');
 });
 
 
