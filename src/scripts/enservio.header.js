0ed110841ad2a2ff51bfa6efab81179d7f0a9b67
Chris Fournier <chris.d.fournier@gmail.com>
2018-10-17 17:52:45 -0400

    header height solution
    Dynamic calculation of <header> height used to set the top padding for <main>; flow updates; Pages index updates.

--- src/scripts/enservio.header.js
+++ src/scripts/enservio.header.js
@@ -3,3 +3,6 @@ var myElement = document.querySelector("header");
 var headroom  = new Headroom(myElement);
 headroom.init(); 
 
+// MAIN PADDING
+var headerHeight = header.offsetHeight;
+$("main").css("padding-top", headerHeight);
